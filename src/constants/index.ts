1e60b856f791bf88ff1767bbfbd441f4966f11e9
hoanganh <anhnh@versehub.io>
2022-08-26 14:33:55 +0700

    update header

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const Anchors = {
 	Services: "services",
 	Products: "products",
-	Partners: "partners",
+	// Partners: "partners",
 	Team: "team",
 	Jobs: "jobs",
 	Contact: "contact",
@@ -10,7 +10,7 @@ export const Anchors = {
 export const navigations = [
 	{ label: "Services", value: "#services" },
 	{ label: "Products", value: "#products" },
-	{ label: "Partners", value: "#partners" },
+	// { label: "Partners", value: "#partners" },
 	// { label: "Team", value: "#team" },
 	{ label: "Jobs", value: "/jobs" },
 	{ label: "Contact", value: "#contact" },
