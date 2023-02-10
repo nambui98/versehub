2838021d453b95ca93d4da9c3710bf67250532fb
hoanganh <anhnh@versehub.io>
2023-02-10 16:09:17 +0700

    update thumb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,7 +33,7 @@ export default function MyApp(props: MyAppProps) {
         <meta property="og:image" content={(pathname === '/cowItUp' || pathname === '/cowItUp-policy') ? "/assets/thumb_ciu.png" : "/assets/og_image.png"} />
 
         
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:title"              
         content={(pathname === '/cowItUp' || pathname === '/cowItUp-policy') ? "Cow it up" : "VerseHub"} />
         <meta property="twitter:description"        
@@ -47,6 +47,7 @@ export default function MyApp(props: MyAppProps) {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         <link rel="icon" href={(pathname === '/cowItUp' || pathname === '/cowItUp-policy') ? "/assets/ciu_favicon.png" : "/assets/versehub_favicon.png"} />
         <link rel="apple-touch-icon" href={(pathname === '/cowItUp' || pathname === '/cowItUp-policy') ? "/assets/ciu_favicon.png" : "/assets/versehub_favicon.png"} />
+        
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
